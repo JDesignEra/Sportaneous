a7bf8957831a15cef4adcb78c24201360db53a53
WILSON\pokem <lim.wilson97@gmail.com>
2017-12-21 01:03:38 +0800

    no message

---
+++ src/dataAccess/facilitiesDA.java
@@ -0,0 +1,33 @@
+package dataAccess;
+
+import java.io.File;
+import java.util.concurrent.ConcurrentMap;
+
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+import entity.FacilitiesEntity;
+
+public class facilitiesDA {
+	private static DB db;
+	private static ConcurrentMap<String, FacilitiesEntity> facilities;
+	private static FacilitiesEntity session;
+
+	public static void initDa() {
+		db = DBMaker.newFileDB(new File("tmp/facilities.db")).closeOnJvmShutdown().make();
+
+		facilities = db.getTreeMap("facilties");
+
+		db.commit();
+	}
+
+	public static Object[][] getAllData() {
+		Object[][] rowData = new Object[facilities.size()][1];
+		int i = 0;
+		for (FacilitiesEntity facilitiesEntity : facilities.values()) {
+			rowData[i][0] = facilitiesEntity.getFacilities();
+			i++;
+		}
+		return rowData;
+	}
+}
