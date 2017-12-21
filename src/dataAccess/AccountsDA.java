74115f29384d43addb398e4c2cea4f609d4afa17
Joel <joel.jdesignera@gmail.com>
2017-12-21 17:41:19 +0800

    [ -- Added -- ] + get session account infos.

--- src/dataAccess/AccountsDA.java
+++ src/dataAccess/AccountsDA.java
@@ -21,37 +21,73 @@ public class AccountsDA {
 				.closeOnJvmShutdown()
 				.make();
 		
-		accounts = db.getTreeMap("accounts");
+		accounts = db.getTreeMap("saccounts");
 		
 		//accounts.put("admin", new AccountsEntity("admin", "admin@nype.edu.sg", "password", "Administrator", "", "", "", "", 0, 0, false, false, new BigDecimal(0), 0, 0, 0));
 		db.commit();
 	}
 	
-	public static Object[][] getAllData() {
-		Object[][] rowData = new Object[accounts.size()][16];
+	public String getName() {
+		return session.getName();
+	}
+	
+	public String getPhoto() {
+		return session.getPhoto();
+	}
+	
+	public String getFavSport() {
+		return session.getFavSport();
+	}
+	
+	public String getInterestedSports() {
+		return session.getInterestedSports();
+	}
+	
+	public String getIntro() {
+		return session.getIntro();
+	}
+	
+	public double getHeight() {
+		return session.getHeight();
+	}
+	
+	public double getWeight() {
+		return session.getHeight();
+	}
+	
+	public boolean getHeightVisibility() {
+		return session.getHeightVisibility();
+	}
+	
+	public BigDecimal getRating() {
+		return session.getRating();
+	}
+	
+	public int getNoRate() {
+		return session.getNoRate();
+	}
+	
+	public int getMatchPlayed() {
+		return session.getMatchPlayed();
+	}
+	
+	public int getTotalMatch() {
+		return session.getTotalMatch();
+	}
+	
+	public static int login(String adminNo, String password) {
+		AccountsEntity accountsEntity = null;
 		
-		int i = 0;
-		for (AccountsEntity accountsEntity : accounts.values()) {
-			rowData[i][0] = accountsEntity.getAdminNo();
-			rowData[i][1] = accountsEntity.getEmail();
-			rowData[i][2] = accountsEntity.getPassword();
-			rowData[i][3] = accountsEntity.getName();
-			rowData[i][4] = accountsEntity.getPhoto();
-			rowData[i][5] = accountsEntity.getFavSport();
-			rowData[i][6] = accountsEntity.getInterestedSports();
-			rowData[i][7] = accountsEntity.getIntro();
-			rowData[i][8] = accountsEntity.getHeight();
-			rowData[i][9] = accountsEntity.getWeight();
-			rowData[i][10] = accountsEntity.getHeightVisibility();
-			rowData[i][11] = accountsEntity.getWeightVisibility();
-			rowData[i][12] = accountsEntity.getRating();
-			rowData[i][13] = accountsEntity.getNoRate();
-			rowData[i][14] = accountsEntity.getMatchPlayed();
-			rowData[i][15] = accountsEntity.getTotalMatch();
-			i++;
+		if (adminNo.isEmpty() || password.isEmpty()) {
+			return 0; // Success
 		}
 		
-		return rowData;
+		if ((accountsEntity = accounts.get(adminNo)) == null) {
+			return 1; // user does not exist
+		}
+		
+		session = accountsEntity;
+		return 3; // Success
 	}
 	
 	public static int addAccount(String adminNo, String email, String password, String name) {
@@ -95,11 +131,41 @@ public class AccountsDA {
 		return 1; // Success
 	}
 	
+	public static void logout() {
+		session = null;
+	}
+	
+	public static Object[][] getAllData() {
+		Object[][] rowData = new Object[accounts.size()][16];
+		
+		int i = 0;
+		for (AccountsEntity accountsEntity : accounts.values()) {
+			rowData[i][0] = accountsEntity.getAdminNo();
+			rowData[i][1] = accountsEntity.getEmail();
+			rowData[i][2] = accountsEntity.getPassword();
+			rowData[i][3] = accountsEntity.getName();
+			rowData[i][4] = accountsEntity.getPhoto();
+			rowData[i][5] = accountsEntity.getFavSport();
+			rowData[i][6] = accountsEntity.getInterestedSports();
+			rowData[i][7] = accountsEntity.getIntro();
+			rowData[i][8] = accountsEntity.getHeight();
+			rowData[i][9] = accountsEntity.getWeight();
+			rowData[i][10] = accountsEntity.getHeightVisibility();
+			rowData[i][11] = accountsEntity.getWeightVisibility();
+			rowData[i][12] = accountsEntity.getRating();
+			rowData[i][13] = accountsEntity.getNoRate();
+			rowData[i][14] = accountsEntity.getMatchPlayed();
+			rowData[i][15] = accountsEntity.getTotalMatch();
+			i++;
+		}
+		
+		return rowData;
+	}
+	
 	public static void main(String args[]) {
 		initDA();
 		
 		for (int i = 0; i < getAllData().length; i++) {
-			int x = 0;
 			for (Object j : getAllData()[i]) {
 				System.out.println(j.toString());
 			}
