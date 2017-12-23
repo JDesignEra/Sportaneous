3368cf4b4344bf9b3eb362326c86959defabd8d3
Goh Kai Hong <gohkaihong95@gmail.com>
2017-12-23 14:33:53 +0800

    [--Reversed--]

--- src/dataAccess/EquipmentDA.java
+++ src/dataAccess/EquipmentDA.java
@@ -12,9 +12,7 @@ import entity.EquipmentsEntity;
 public class EquipmentDA {
 	
 	private static DB db;
-	private static DB db1;
 	private static ConcurrentMap<String, EquipmentsEntity> equipments;
-	private static ConcurrentMap<String, Integer> stock;
 	
 	public static void initDA() {
 		db = DBMaker
@@ -22,14 +20,7 @@ public class EquipmentDA {
 				.closeOnJvmShutdown()
 				.make();
 		
-		db1 = DBMaker
-				.newFileDB(new File("tmp/equipmentStock.db"))
-				.closeOnJvmShutdown()
-				.make();
-		
 		equipments = db.getTreeMap("equipments");
-		
-		stock = db1.getTreeMap("tmp/equipmentStock.db");
 		
 		
 		if (equipments.keySet().isEmpty()) {
@@ -40,20 +31,9 @@ public class EquipmentDA {
 			equipments.put("squash", new EquipmentsEntity("Squash", 100));
 			equipments.put("tennis", new EquipmentsEntity("Tennis", 100));
 		}
-		
-		if (stock.keySet().isEmpty()) {
-			stock.put("badminton", 100);
-			stock.put("basketball", 100);
-			stock.put("frisbee", 100);
-			stock.put("soccer", 100);
-			stock.put("squash", 100);
-			stock.put("tennis", 100);
-		}
-		
-
-		
+			
 		db.commit();
-		db1.commit();
+
 	}
 	
 	public static int rentEquipment(String sport) {
@@ -72,22 +52,16 @@ public class EquipmentDA {
 	}
 	
 	public static int returnEquipment(String sport) {
-		int equipmentQty = equipments.get(sport).getEquipmentQty();
 		
-		if (equipmentQty+1 <= stock.get(sport)) {
-			equipments.replace(sport, new EquipmentsEntity(sport, ++equipmentQty));
-		} 
+		int equipmentQty = equipments.get(sport).getEquipmentQty();
+	
+		equipments.replace(sport, new EquipmentsEntity(sport, ++equipmentQty));
 		
 		db.commit();
 		
 		return equipmentQty;
 	}
 	
-	public static int checkStock(String sport) {
-		
-		return stock.get(sport) - equipments.get(sport).getEquipmentQty();
-		
-	}
 	
 	public static Object[][] getAllData() {
 		Object[][] data = new Object[equipments.size()][2];
@@ -104,35 +78,27 @@ public class EquipmentDA {
 		return data;
 	}
 	
-	public static void addStock(String sport, int no) {
+	public static void addEquipment(String sport, int no) {
 		
 		if (no > 0) {
-			int newno = stock.get(sport) + no;
-			stock.replace(sport, newno);
-			int newno1 = equipments.get(sport).getEquipmentQty() + no;
-			equipments.replace(sport, new EquipmentsEntity(sport, newno1));
+			
+			equipments.replace(sport, new EquipmentsEntity(sport, equipments.get(sport).getEquipmentQty()+no));
 			
 			db.commit();
-			db1.commit();
 		}
-		
-		System.out.print("Total stock for " + sport + " after adding: " + stock.get(sport));
+
 	}
 	
-	public static void removeStock(String sport, int no) {
+	public static void removeEquipment(String sport, int no) {
 		
 		if (no > 0) {
 			if (equipments.get(sport).getEquipmentQty() - no >= 0) {
-				int newno = equipments.get(sport).getEquipmentQty() - no;
-				equipments.replace(sport, new EquipmentsEntity(sport, newno));
-				stock.replace(sport, stock.get(sport)-no);
+				
+				equipments.replace(sport, new EquipmentsEntity(sport, equipments.get(sport).getEquipmentQty()-no));
 				
 				db.commit();
-				db1.commit();
 			}
 		}
-		
-		System.out.println("Total stock for " + sport + " after removing: " + stock.get(sport));
 		
 	}
 	
